d367f62beac0cc57ba52c0a109367f90b1a2bba2
daysesa <daysesa@gmail.com>
2020-03-27 21:31:02 -0300

    Front-end do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './global.css';
+
+//import Logon from './pages/Logon'; //não precisa importar index porque essa procura é automática
+import Routes from './routes';
 
 function App() {
+
+  /*
+  const [counter, setCounter] = useState(0); // retorna uma Array de duas posições [valor, funcaoDeAtualizacao]
+
+  function increment(){
+    setCounter(counter + 1);
+
+//    console.log(counter);
+  }
+*/
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //<h1>Hello World</h1>
+    /*<div>
+    <Header>Contador: {counter}</Header>
+    <button onClick={increment}>Incrementar</button>
+    </div>*/
+    //<Logon />
+    <Routes />
   );
 }
 
